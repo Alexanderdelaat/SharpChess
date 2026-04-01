f87368f2eef8e4a2e1ff289abbd36cd159ca68cb
Alexander <alexander@Alexanders-MacBook-Air.local>
2026-04-01 12:11:03 +0200

    front-end

--- src/SharpChess.Api/Program.cs
+++ src/SharpChess.Api/Program.cs
@@ -8,6 +8,16 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Frontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 
 WebApplication app = builder.Build();
 
@@ -16,8 +26,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-// app.UseHttpsRedirection();
 app.UseAuthentication();
+app.UseCors("Frontend");
 app.UseAuthorization();
 app.MapControllers();
 
